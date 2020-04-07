9d22ce9b0d8cc9d094add0398d0ad437cba66639
WindBeast <62758748+WindBeast@users.noreply.github.com>
2020-04-07 11:45:34 +0900

    Add files via upload

---
+++ PHPtest.php
@@ -0,0 +1,14 @@
+<!DOCTYPE HTML>
+<HTML>
+<HEAD>
+    <TITLE></TITLE>
+    <meta name="viewport" content="width=device-width>
+    <META HTTP-EQUIV="Content-Type" CONTENT="text/html; charset=utf-8">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+</HEAD>
+<BODY>
+<?php
+echo "<h1>Hello,PHP!</h1>";
+?>
+</BODY>
+</HTML>
